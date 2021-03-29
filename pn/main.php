5a8f02be88c0987471e19ee209f75d86cce4e566
Kbakz <68775535+Kbakz@users.noreply.github.com>
2021-03-29 15:37:42 -0300

    Update main.php

--- pn/main.php
+++ pn/main.php
@@ -34,6 +34,7 @@
 			<h3>Gerenciar usuarios</h3>
 			<a <?php Site::selectedMenu('adicionar-usuario'); Permissao::verificaPermissaoMenu(0)?> href="<?php echo INCLUDE_PATH_PAINEL?>adicionar-usuario">Adicionar novo usuario</a>
 			<a <?php Site::selectedMenu('usuarios-cadastrados'); Permissao::verificaPermissaoMenu(1)?> href="<?php echo INCLUDE_PATH_PAINEL?>usuarios-cadastrados">Usuarios cadastrados</a>
+			<a <?php Site::selectedMenu('editar-perfil');?> href="<?php echo INCLUDE_PATH_PAINEL?>editar-perfil">Editar perfil</a>
 		</div><!--itens-menu-->
 	</div><!--menu-painel-->
 	<div class="header-painel">
