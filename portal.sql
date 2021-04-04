5883277991ccf0a505f8f42194ae7be394c44601
Kbakz <68775535+Kbakz@users.noreply.github.com>
2021-04-03 21:07:50 -0300

    Update portal.sql

--- portal.sql
+++ portal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 31-Mar-2021 às 01:43
+-- Tempo de geração: 04-Abr-2021 às 02:07
 -- Versão do servidor: 10.4.17-MariaDB
 -- versão do PHP: 8.0.2
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `tb_admin.apoiadores`
+--
+
+CREATE TABLE `tb_admin.apoiadores` (
+  `id` int(11) NOT NULL,
+  `imagem` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tb_admin.apoiadores`
+--
+
+INSERT INTO `tb_admin.apoiadores` (`id`, `imagem`, `nome`, `link`) VALUES
+(2, '6066357932028.png', 'Americanas', 'americana.com'),
+(7, '606635e33901a.png', 'Casas bahia', 'casasbahia.com');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `tb_admin.categoria`
 --
 
@@ -38,9 +59,36 @@ CREATE TABLE `tb_admin.categoria` (
 --
 
 INSERT INTO `tb_admin.categoria` (`id`, `nome`, `slug`) VALUES
-(1, 'Esporte ', 'esporte-'),
-(3, 'Saúde', 'saude'),
-(6, 'Automóveis', 'automoveis');
+(10, 'Carros', 'carros'),
+(11, 'Avião', 'aviao'),
+(12, 'Moto', 'moto');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_admin.classificados`
+--
+
+CREATE TABLE `tb_admin.classificados` (
+  `id` int(11) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `horario_abrir` varchar(255) NOT NULL,
+  `horario_fechar` varchar(255) NOT NULL,
+  `dias` varchar(255) NOT NULL,
+  `telefone` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tb_admin.classificados`
+--
+
+INSERT INTO `tb_admin.classificados` (`id`, `img`, `nome`, `horario_abrir`, `horario_fechar`, `dias`, `telefone`, `link`) VALUES
+(12, '6066344ca8b17.jpg', 'Americanas', '08:00', '22:00', 'Segunda a sexta', '(11) 99999-9999', 'americanas.com'),
+(13, '6066345c09766.png', 'Casas bahia', '08:00', '20:00', 'Segunda a sabado', '(11) 99999-9999', 'casasbahia.com'),
+(15, '60663481472d0.png', 'Magalu', '08:00', '20:00', 'Segunda a sexta', '(11) 99999-9999', 'magalu.com'),
+(16, '60663470ea668.jpg', 'Lojas cem', '10:00', '22:00', 'Segunda a sabado', '(11) 99999-9999', 'lojacem.com');
 
 -- --------------------------------------------------------
 
@@ -56,6 +104,7 @@ CREATE TABLE `tb_admin.noticias` (
   `imagem` varchar(255) NOT NULL,
   `data` date NOT NULL,
   `autor` varchar(255) NOT NULL,
+  `destaque` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -63,10 +112,10 @@ CREATE TABLE `tb_admin.noticias` (
 -- Extraindo dados da tabela `tb_admin.noticias`
 --
 
-INSERT INTO `tb_admin.noticias` (`id`, `categoria_id`, `titulo`, `conteudo`, `imagem`, `data`, `autor`, `slug`) VALUES
-(1, 1, 'Notícia Esporte', 'conteudo atualizado', '60633e27b3a43.jpg', '2021-03-30', 'Kevin Freire', 'noticia-esporte'),
-(2, 3, 'Notícia saúde', 'conteudo noticia saúde', '60634c7ecd461.jpg', '2021-03-30', 'Kevin Freire', 'noticia-saude'),
-(3, 6, 'Tesla ', 'o carro mais foda', '60634dfc38605.png', '2021-03-30', 'freire', 'tesla-');
+INSERT INTO `tb_admin.noticias` (`id`, `categoria_id`, `titulo`, `conteudo`, `imagem`, `data`, `autor`, `destaque`, `slug`) VALUES
+(18, 10, 'Um carro foda', '<p><span style=\"color: #656565; font-family: \'Varela Round\', calibri, sans-serif; text-align: justify; background-color: #ffffff;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus velit ligula, eget egestas massa consectetur ut. Sed quis neque felis. Phasellus at rhoncus libero, eu lobortis est. Suspendisse malesuada mi sit amet leo tincidunt, quis euismod diam rhoncus. Sed vehicula tincidunt rutrum. Nam commodo maximus augue vel sagittis. Vestibulum viverra consequat augue sed vehicula. Suspendisse quam augue, faucibus eget imperdiet congue, aliquet eget dui. Duis suscipit odio orci. Aenean at fringilla lectus. Aenean nibh neque, ultricies ac libero eu, luctus condimentum augue. Maecenas et vehicula</span></p>', '6066470f8e127.jpg', '2021-04-01', 'Kevin Freire', 'true', 'um-carro-foda'),
+(19, 11, 'Avião', '<p style=\"margin: 0px 0px 15px; padding: 0px; text-align: justify; font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; background-color: #ffffff;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus velit ligula, eget egestas massa consectetur ut. Sed quis neque felis. Phasellus at rhoncus libero, eu lobortis est. Suspendisse malesuada mi sit amet leo tincidunt, quis euismod diam rhoncus. Sed vehicula tincidunt rutrum. Nam commodo maximus augue vel sagittis. Vestibulum viverra consequat augue sed vehicula. Suspendisse quam augue, faucibus eget imperdiet congue, aliquet eget dui. Duis suscipit odio orci. Aenean at fringilla lectus. Aenean nibh neque, ultricies ac libero eu, luctus condimentum augue. Maecenas et vehicula turpis.</p>\r\n<p style=\"margin: 0px 0px 15px; padding: 0px; text-align: justify; font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; background-color: #ffffff;\">Cras tempor volutpat semper. Nulla tincidunt, ex scelerisque egestas vulputate, justo sapien feugiat diam, at placerat massa mi ac metus. Nunc ex nisl, laoreet at mauris eget, iaculis mollis nibh. Curabitur sed tempus magna. Vestibulum lacinia, urna in posuere gravida, dolor ante ullamcorper felis, et euismod neque leo et ligula. Nullam porta lacinia turpis, sed mollis diam volut</p>\r\n<p style=\"margin: 0px 0px 15px; padding: 0px; text-align: justify; font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; background-color: #ffffff;\">&nbsp;</p>\r\n<p style=\"margin: 0px 0px 15px; padding: 0px; text-align: justify; font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; background-color: #ffffff;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus velit ligula, eget egestas massa consectetur ut. Sed quis neque felis. Phasellus at rhoncus libero, eu lobortis est. Suspendisse malesuada mi sit amet leo tincidunt, quis euismod diam rhoncus. Sed vehicula tincidunt rutrum. Nam commodo maximus augue vel sagittis. Vestibulum viverra consequat augue sed vehicula. Suspendisse quam augue, faucibus eget imperdiet congue, aliquet eget dui. Duis suscipit odio orci. Aenean at fringilla lectus. Aenean nibh neque, ultricies ac libero eu, luctus condimentum augue. Maecenas et vehicula turpis.</p>\r\n<p style=\"margin: 0px 0px 15px; padding: 0px; text-align: justify; font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; background-color: #ffffff;\">Cras tempor volutpat semper. Nulla tincidunt, ex scelerisque egestas vulputate, justo sapien feugiat diam, at placerat massa mi ac metus. Nunc ex nisl, laoreet at mauris eget, iaculis mollis nibh. Curabitur sed tempus magna. Vestibulum lacinia, urna in posuere gravida, dolor ante ullamcorper felis, et euismod neque leo et ligula. Nullam porta lacinia turpis, sed mollis diam volut</p>', '6066386385f9e.jpeg', '2021-04-01', 'Kevin Freire', 'false', 'aviao'),
+(20, 12, 'Moto mais foda que tem', '<p style=\"margin: 0px 0px 15px; padding: 0px; text-align: justify; font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; background-color: #ffffff;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus velit ligula, eget egestas massa consectetur ut. Sed quis neque felis. Phasellus at rhoncus libero, eu lobortis est. Suspendisse malesuada mi sit amet leo tincidunt, quis euismod diam rhoncus. Sed vehicula tincidunt rutrum. Nam commodo maximus augue vel sagittis. Vestibulum viverra consequat augue sed vehicula. Suspendisse quam augue, faucibus eget imperdiet congue, aliquet eget dui. Duis suscipit odio orci. Aenean at fringilla lectus. Aenean nibh neque, ultricies ac libero eu, luctus condimentum augue. Maecenas et vehicula turpis.</p>\r\n<p style=\"margin: 0px 0px 15px; padding: 0px; text-align: justify; font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; background-color: #ffffff;\">Cras tempor volutpat semper. Nulla tincidunt, ex scelerisque egestas vulputate, justo sapien feugiat diam, at placerat massa mi ac metus. Nunc ex nisl, laoreet at mauris eget, iaculis mollis nibh. Curabitur sed tempus magna. Vestibulum lacinia, urna in posuere gravida, dolor ante ullamcorper felis, et euismod neque leo et ligula. Nullam porta lacinia turpis, sed mollis diam volutLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus velit ligula, eget egestas massa consectetur ut. Sed quis neque felis. Phasellus at rhoncus libero, eu lobortis est. Suspendisse malesuada mi sit amet leo tincidunt, quis euismod diam rhoncus. Sed vehicula tincidunt rutrum. Nam commodo maximus augue vel sagittis. Vestibulum viverra consequat augue sed vehicula. Suspendisse quam augue, faucibus eget imperdiet congue, aliquet eget dui. Duis suscipit odio orci. Aenean at fringilla lectus. Aenean nibh neque, ultricies ac libero eu, luctus condimentum augue. Maecenas et vehicula turpis.</p>\r\n<p style=\"margin: 0px 0px 15px; padding: 0px; text-align: justify; font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; background-color: #ffffff;\">Cras tempor volutpat semper. Nulla tincidunt, ex scelerisque egestas vulputate, justo sapien feugiat diam, at placerat massa mi ac metus. Nunc ex nisl, laoreet at mauris eget, iaculis mollis nibh. Curabitur sed tempus magna. Vestibulum lacinia, urna in posuere gravida, dolor ante ullamcorper felis, et euismod neque leo et ligula. Nullam porta lacinia turpis, sed mollis diam volutLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus velit ligula, eget egestas massa consectetur ut. Sed quis neque felis. Phasellus at rhoncus libero, eu lobortis est. Suspendisse malesuada mi sit amet leo tincidunt, quis euismod diam rhoncus. Sed vehicula tincidunt rutrum. Nam commodo maximus augue vel sagittis. Vestibulum viverra consequat augue sed vehicula. Suspendisse quam augue, faucibus eget imperdiet congue, aliquet eget dui. Duis suscipit odio orci. Aenean at fringilla lectus. Aenean nibh neque, ultricies ac libero eu, luctus condimentum augue. Maecenas et vehicula turpis.</p>\r\n<p style=\"margin: 0px 0px 15px; padding: 0px; text-align: justify; font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; background-color: #ffffff;\">Cras tempor volutpat semper. Nulla tincidunt, ex scelerisque egestas vulputate, justo sapien feugiat diam, at placerat massa mi ac metus. Nunc ex nisl, laoreet at mauris eget, iaculis mollis nibh. Curabitur sed tempus magna. Vestibulum lacinia, urna in posuere gravida, dolor ante ullamcorper felis, et euismod neque leo et ligula. Nullam porta lacinia turpis, sed mollis diam volut</p>', '6066471cf3863.jpg', '2021-04-01', 'Kevin Freire', 'false', 'moto-mais-foda-que-tem');
 
 -- --------------------------------------------------------
 
@@ -80,6 +129,13 @@ CREATE TABLE `tb_admin.online` (
   `ultima_acao` datetime NOT NULL,
   `token` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tb_admin.online`
+--
+
+INSERT INTO `tb_admin.online` (`id`, `ip`, `ultima_acao`, `token`) VALUES
+(48, '::1', '2021-04-03 18:18:26', '6068c26c64a09');
 
 -- --------------------------------------------------------
 
@@ -101,10 +157,7 @@ CREATE TABLE `tb_admin.usuarios` (
 --
 
 INSERT INTO `tb_admin.usuarios` (`id`, `usuario`, `senha`, `img`, `nome`, `cargo`) VALUES
-(11, 'admin', 'admin', '6062511bcdd74.png', 'Kevin Freire', 0),
-(15, 'Gustavo', 'senha', '60620bb1a8c0b.jpg', 'Gustavo Silva', 1),
-(16, 'kevin freire', 'senha', '60620c0f0a39e.jpg', 'freire', 2),
-(17, 'editor', 'senha', '60620c8c15f60.jpg', 'Algum nome', 3);
+(11, 'admin', 'admin', '60661cfda5411.png', 'Kevin Freire', 0);
 
 -- --------------------------------------------------------
 
@@ -124,16 +177,55 @@ CREATE TABLE `tb_admin.visitas` (
 
 INSERT INTO `tb_admin.visitas` (`id`, `ip`, `data`) VALUES
 (2, '::1', '2021-03-28'),
-(3, '::1', '2021-03-28');
+(3, '::1', '2021-03-28'),
+(4, '::1', '2021-03-31'),
+(5, '127.0.0.1', '2021-03-31');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_site.config`
+--
+
+CREATE TABLE `tb_site.config` (
+  `id` int(11) NOT NULL,
+  `img_sobre` varchar(255) NOT NULL,
+  `texto_sobre` text NOT NULL,
+  `icone1` varchar(255) NOT NULL,
+  `social_1` varchar(255) NOT NULL,
+  `icone2` varchar(255) NOT NULL,
+  `social_2` varchar(255) NOT NULL,
+  `icone3` varchar(255) NOT NULL,
+  `social_3` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tb_site.config`
+--
+
+INSERT INTO `tb_site.config` (`id`, `img_sobre`, `texto_sobre`, `icone1`, `social_1`, `icone2`, `social_2`, `icone3`, `social_3`) VALUES
+(1, '60663639addfd.png', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus velit ligula, eget egestas massa consectetur ut. Sed quis neque felis. Phasellus at rhoncus libero, eu lobortis est. Suspendisse malesuada mi sit amet leo tincidunt, quis euismod diam rhoncus. Sed vehicula tincidunt rutrum. Nam commodo maximus augue vel sagittis. Vestibulum viverra consequat augue sed vehicula. Suspendisse quam augue, faucibus eget imperdiet congue, aliquet eget dui. Duis suscipit odio orci. Aenean at fringilla lectus. Aenean nibh neque, ultricies ac libero eu, luctus condimentum augue. Maecenas et vehicula turpis.\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus velit ligula, eget egestas massa consectetur ut. Sed quis neque felis. Phasellus at rhoncus libero, eu lobortis est. Suspendisse malesuada mi sit amet leo tincidunt, quis euismod diam rhoncus. Sed vehicula tincidunt rutrum. Nam commodo maximus augue vel sagittis. Vestibulum viverra consequat augue sed vehicula. Suspendisse quam augue, faucibus eget imperdiet congue, aliquet eget dui. Duis suscipit odio orci. Aenean at fringilla lectus. Aenean nibh neque, ultricies ac libero eu, luctus condimentum augue. Maecenas et vehicula turpis.', 'fab fa-facebook-f', 'facebook.com', 'fab fa-twitter', 'https://twitter.com', 'fab fa-instagram', 'instagram.com');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
+-- Índices para tabela `tb_admin.apoiadores`
+--
+ALTER TABLE `tb_admin.apoiadores`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices para tabela `tb_admin.categoria`
 --
 ALTER TABLE `tb_admin.categoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_admin.classificados`
+--
+ALTER TABLE `tb_admin.classificados`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -161,38 +253,62 @@ ALTER TABLE `tb_admin.visitas`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `tb_site.config`
+--
+ALTER TABLE `tb_site.config`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `tb_admin.apoiadores`
+--
+ALTER TABLE `tb_admin.apoiadores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `tb_admin.categoria`
 --
 ALTER TABLE `tb_admin.categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de tabela `tb_admin.classificados`
+--
+ALTER TABLE `tb_admin.classificados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de tabela `tb_admin.noticias`
 --
 ALTER TABLE `tb_admin.noticias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de tabela `tb_admin.online`
 --
 ALTER TABLE `tb_admin.online`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT de tabela `tb_admin.usuarios`
 --
 ALTER TABLE `tb_admin.usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de tabela `tb_admin.visitas`
 --
 ALTER TABLE `tb_admin.visitas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `tb_site.config`
+--
+ALTER TABLE `tb_site.config`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
