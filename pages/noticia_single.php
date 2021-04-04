62f705e09127e1100680ef971104afcc96010268
Lone-lima <eatalone20@gmail.com>
2021-04-04 00:47:25 -0300

    Update noticia_single.php

--- pages/noticia_single.php
+++ pages/noticia_single.php
@@ -1,1 +1,35 @@
-Noticia single
+<div class="box-content">
+    <div class="center">
+    	<div class="post">
+    		<div class="titulo-site">
+    			<h1>Título</h1>
+    		</div>
+    		<p>
+    			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt in erat vitae tristique. Fusce ac sollicitudin nunc. Pellentesque molestie at nibh tincidunt auctor. Suspendisse lorem turpis.
+    		</p>
+    		<div class="post-autor">
+    			<p>Por: <b>Kevin F.</b><a href=""><i class="fas fa-share-alt"></i></a></p>
+    			<data>03/04/2021</data>
+    		</div>
+    		<hr>
+    		<img src="<?php echo INCLUDE_PATH_PAINEL?>uploads/6066386385f9e.jpeg">
+    		<p>fonte: gol aviação</p>
+
+    		<article class="conteudo-post">
+    			<p>
+    				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt in erat vitae tristique. Fusce ac sollicitudin nunc. Pellentesque molestie at nibh tincidunt auctor. Suspendisse lorem turpis, tristique vel elementum sit amet, dapibus id libero. Duis pulvinar ligula ex, vel consectetur mauris imperdiet vitae. Nulla vel nibh in ipsum lacinia porta. Morbi sollicitudin porttitor risus. Vestibulum mollis dolor dapibus dolor sollicitudin, vel tempus libero ullamcorper. Duis molestie scelerisque porttitor. Sed finibus purus nec lorem porta, non hendrerit lacus placerat. Aliquam fringilla egestas egestas. Curabitur eget ante sit amet lacus sodales dictum.
+
+					Donec finibus mi id mi ultricies molestie. Sed ornare lacus vitae sapien placerat, sit amet fringilla quam posuere. Praesent varius congue blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla auctor, nulla sed euismod tempor, est massa porttitor lectus, eu interdum arcu arcu ac ante. Donec arcu magna, molestie in mattis ac, porttitor nec tortor. Maecenas accumsan dignissim urna, non mollis odio interdum eu. Ut scelerisque tellus risus, vel commodo tellus bibendum ac. Praesent sed leo quis tortor pharetra gravida eu sit amet enim. Pellentesque maximus ac nisl malesuada consectetur.
+
+					Morbi suscipit, urna sed vulputate varius, dui leo accumsan metus, eu congue dui nisi vel sem. Aliquam metus leo, tristique vitae lacinia congue, posuere vitae urna. Vivamus sit amet augue vitae leo tempus fringilla id nec dolor. Curabitur eu neque turpis. Etiam at mauris at metus fringilla consectetur. Pellentesque in faucibus urna, in imperdiet ipsum. Sed sit amet enim urna. Nunc vel metus sit amet arcu dictum luctus. Nunc id urna erat.
+    			</p>
+    			<img src="<?php echo INCLUDE_PATH_PAINEL?>uploads/606674ec7c033.jpg">
+    			<p>
+    				Donec finibus mi id mi ultricies molestie. Sed ornare lacus vitae sapien placerat, sit amet fringilla quam posuere. Praesent varius congue blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla auctor, nulla sed euismod tempor, est massa porttitor lectus, eu interdum arcu arcu ac ante. Donec arcu magna, molestie in mattis ac, porttitor nec tortor. Maecenas accumsan dignissim urna, non mollis odio interdum eu. Ut scelerisque tellus risus, vel commodo tellus bibendum ac. Praesent sed leo quis tortor pharetra gravida eu sit amet enim. Pellentesque maximus ac nisl malesuada consectetur.
+
+					Morbi suscipit, urna sed vulputate varius, dui leo accumsan metus, eu congue dui nisi vel sem. Aliquam metus leo, tristique vitae lacinia congue, posuere vitae urna. Vivamus sit amet augue vitae leo tempus fringilla id nec dolor. Curabitur eu neque turpis. Etiam at mauris at metus fringilla consectetur. Pellentesque in faucibus urna, in imperdiet ipsum. Sed sit amet enim urna. Nunc vel metus sit amet arcu dictum luctus. Nunc id urna erat.
+    			</p>
+    		</article>
+    	</div>
+    </div>
+</div><!--box-content-->
