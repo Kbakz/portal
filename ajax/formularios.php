0fd38776fd2140066bd1f20c21f420f587119d8e
Kbakz <kevingustavo14@gmail.com>
2021-04-19 10:30:09 -0300

    Create formularios.php

---
+++ ajax/formularios.php
@@ -0,0 +1,25 @@
+<?php
+	include('../config.php');
+	$data = array();
+	$assunto = 'Novo mensagem do site!';
+	$corpo = '';
+	foreach ($_POST as $key => $value) {
+		$corpo.=ucfirst($key).": ".$value;
+		$corpo.="<hr>";
+	}
+	$info = array('assunto'=>$assunto,'corpo'=>$corpo);
+	$mail = new Email('portfoliokevinfreire.com','contato@portfoliokevinfreire.com','1245K&vin','Kevin');
+	$mail->addAdress('contato@portfoliokevinfreire.com','Guilherme');
+	$mail->formatarEmail($info);
+	if($mail->enviarEmail()){
+		$data['sucesso'] = true;
+	}else{
+		$data['erro'] = true;
+	}
+
+
+
+	//$data['retorno'] = 'sucesso';
+
+	die(json_encode($data));
+?>
