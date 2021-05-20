fc0d5dc9e2209a5dd209e364d1226775eecfe8df
Lone-lima <eatalone20@gmail.com>
2021-05-20 13:55:41 -0300

    Create main.js

---
+++ ajax/pn/js/main.js
@@ -0,0 +1,63 @@
+$(function(){
+	fecharMenu();
+	//limitarCaracter();
+
+	function fecharMenu(){
+		$('p.btn-menu').click(function(){
+			var menuPainel = $('.menu-painel');
+			var offSet = menuPainel.offset().left;
+			var botao = $('p.btn-menu');
+			var icone = $('p.btn-menu > i');
+
+			if (offSet == 0) {
+				//fechar menu
+				menuPainel.animate({'left':'-300px'});
+				botao.animate({'left':'0px'});
+				icone.removeClass('fa-angle-double-left');
+				icone.addClass('fa-angle-double-right');
+			}else{
+				//abrir menu
+				menuPainel.animate({'left':'0'});
+				botao.animate({'left':'300px'});
+				icone.removeClass('fa-angle-double-right');
+				icone.addClass('fa-angle-double-left');
+			}
+		})
+		
+	}
+
+	function limitarCaracter(){
+		var campo = $('#limitar');
+		var contador = $('span.contador');
+		var campoValor = campo.val();
+		var quant = campoValor.length;
+		var limite = 600;
+		contador.html('<span>'+quant+'/'+limite+'</span>');
+		quant = campoValor.length;
+		campo.keyup(function(){
+			campoValor = campo.val();
+			quant = campoValor.length;
+			contador.html('<span>'+quant+'/'+limite+'</span>');
+			if(quant >= limite){
+				contador.css('color' , 'red');
+				campo.keypress(function(){
+					if(quant >= limite)
+						return false;
+					else
+						return true;
+				})
+			}else{
+				contador.css('color' , '#646464');
+			}
+		})
+		
+	}
+
+	tinymce.init({
+        selector: '#tinymce',
+        plugins: 'image imagetools emoticons'
+      });
+
+	 $('.horario').mask('00:00');
+	 $('.telefone').mask('(00) 00000-0000');
+})
