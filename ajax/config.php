961c640615a96dd00516a4104e275f16bb55cca8
Lone-lima <eatalone20@gmail.com>
2021-05-20 13:57:28 -0300

    Create config.php

---
+++ ajax/config.php
@@ -0,0 +1,45 @@
+<?php
+	session_start();
+	date_default_timezone_set('America/Sao_Paulo');
+	/*
+	$autoload = function($class){
+		if($class == 'Email'){
+			require_once('classes/phpmailer/PHPMailerAutoload.php');
+		}
+
+		include('classes/'.$class.'.php');
+		
+	};
+	
+	spl_autoload_register($autoload);
+	*/
+
+	 function MyAutoload($class){
+		include('classes/'.$class.'.php');
+	};
+
+	spl_autoload_register('MyAutoload');
+
+	//local
+	///*
+	define('INCLUDE_PATH', 'http://localhost/Portal/');
+
+	define('HOST', 'localhost');
+	define('DATABASE', 'portal');
+	define('USUARIO', 'root');
+	define('SENHA', '');
+	//*/
+	
+	//Servidor
+	/*
+	define('INCLUDE_PATH', 'http://portal.portfoliokevinfreire.com/');
+
+	define('HOST', 'localhost');
+	define('DATABASE', 'portif49_portal');
+	define('USUARIO', 'portif49_kevin');
+	define('SENHA', '18p3wFe3uB');
+	*/
+
+	define('INCLUDE_PATH_PAINEL', INCLUDE_PATH.'pn/');
+	define('BASE_DIR_PAINEL',__DIR__.'/pn');
+?>
