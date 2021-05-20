6ab59c92c280acd2f5c32f0e9761c4dc52bb55ad
Lone-lima <eatalone20@gmail.com>
2021-05-20 13:48:08 -0300

    Create gerenciar-classificados.php

---
+++ ajax/pn/pages/gerenciar-classificados.php
@@ -0,0 +1,62 @@
+<?php
+	Permissao::verificaPermissaoPagina(1);
+	$tabela = 'tb_admin.classificados';
+	if(isset($_GET['deletar'])){
+		$idExcluir = intval($_GET['deletar']);
+		$selecionarImg = MySql::conectar()->prepare("SELECT img FROM `$tabela` WHERE id = ?");
+		$selecionarImg->execute(array($idExcluir));
+		$selecionarImg = $selecionarImg->fetch();
+		Painel::deleteFile($selecionarImg['img']);
+		Painel::deletar($tabela,$idExcluir);
+		header('location:'.INCLUDE_PATH_PAINEL.'gerenciar-classificados');
+		die();
+	}
+
+	$paginaAtual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+	$porPagina = 15;
+	$query = ($paginaAtual - 1) * $porPagina;
+?>
+<div class="conteudo-painel">
+	<h2>Classificados cadastrados</h2>
+
+	<div class="table-overflow"> 
+		<table> 
+			<tr> 
+				<th>Nome</th>
+				<th>#</th> 
+				<th>#</th> 
+			</tr> 
+			<?php
+				$sql = MySql::conectar()->prepare("SELECT * FROM `$tabela` ORDER BY nome LIMIT $query,$porPagina"); 
+				$sql->execute(); 
+				$classificados = $sql->fetchAll();
+		
+				foreach ($classificados as $key => $value) {
+
+			?>
+			<tr>
+				<td><?php echo $value['nome']?></td>
+				<td><a class="btn editar" href="<?php echo INCLUDE_PATH_PAINEL?>editar-classificado?id=<?php echo $value['id']?>">Editar</a></td>
+				<td><a class="btn excluir" href="<?php echo INCLUDE_PATH_PAINEL?>gerenciar-classificados?deletar=<?php echo $value['id']?>">Excluir</a></td>
+			</tr>
+			<?php }?>
+		</table>
+	</div><!--table-overflow-->
+	<div class="paginacao">
+		<?php
+			$sql = MySql::conectar()->prepare("SELECT * FROM `$tabela`");
+			$sql->execute();
+			$sql = $sql->fetchAll();
+			$totalPaginas = ceil(count($sql) / $porPagina);
+
+			for($i = 1; $i <= $totalPaginas; $i++){
+				if($totalPaginas != 1){
+					if( $i == $paginaAtual)
+						echo '<a class="selecionado" href="'.INCLUDE_PATH_PAINEL.'/gerenciar-classificados?pagina='.$i.'">'.$i.'</a>';
+					else
+						echo '<a href="'.INCLUDE_PATH_PAINEL.'/gerenciar-classificados?pagina='.$i.'">'.$i.'</a>';
+				}
+			}
+		?>
+	</div><!--paginacao-->
+</div><!--conteudo-painel-->
